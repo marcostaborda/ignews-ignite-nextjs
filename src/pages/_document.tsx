e9045fdc283498d14ed8e54b29525d6fa4e028bf
Marcos Taborda <marcos.tabordamail@gmail.com>
2021-04-18 22:02:14 -0300

    feat(component) - add header component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,15 +4,16 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
       </Html>
     )
   }
