ceaa0cb45ed0e074bba4437bd86848ad3c4d13a4
Marcos Taborda <marcos.tabordamail@gmail.com>
2021-05-09 01:31:20 -0300

    feat - add faunadb and stripe payment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product : { 
+    priceID: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -15,13 +24,32 @@ export default function Home() {
           <h1>News about the <span>React</span> world. </h1>
           <p>
             Get Access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceID}/>
         </section>
 
         <img src="/images/avatar.svg" alt="Girl Coding" />
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Iore0DAbgiDKjLiSea4dWGf')
+
+  const product = { 
+    priceID: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      currency: 'USD',
+      style: 'currency'
+    }).format(price.unit_amount / 100)
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60* 60 * 24 // milliseconds 24hour
+  }
 }
